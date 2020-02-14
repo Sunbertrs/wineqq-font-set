6d37a99442e9fdeb1b67f4abc9175bc034e43796
SunbossRS <3127723014@qq.com>
2020-02-14 17:18:55 +0800

    Release

---
+++ StartTIM.sh
@@ -0,0 +1,1 @@
+wine ~/.wine/drive_c/Program\ Files\ \(x86\)/Tencent/TIM/Bin/TIM.exe
--- zhfont.sh
+++ zhfont.sh
@@ -1,14 +1,14 @@
 echo "[+] 现在这个脚本将自动配置Wine的中文字体。请稍候...\n[+] This script is setting up the Chinese font of Wine. Please wait..."
-sleep 2
+sleep 3
 clear
-wget https://github.com/SunbossRS/chinese-wine-configure/blob/master/fontlib/msyh.ttf?raw=true
-wget https://github.com/sunbossrs/chinese-wine-configure/blob/master/fontlib/msyhbd.ttf?raw=true
+wget https://github.com/sunbossrs/chinese-wine-configure/blob/master/fontlib/msyh.ttf?raw=true
+wget https://github.com/sunbossrs/chinese-wine-configure/blob/master/fontlib/simsum.ttc?raw=true
 wget https://github.com/sunbossrs/chinese-wine-configure/blob/master/zhfont.reg?raw=true
-cp ./msyh.ttf ~/.wine/drive_c/windows/Fonts/
-cp ./msyhbd.ttf ~/.wine/drive_c/windows/Fonts/
-wine regedit /c zhfont.reg
-rm ./msyh.ttf
-rm ./msyhbd.ttf
-rm ./zhfont.reg
+cp msyh.ttf ~/.wine/drive_c/windows/Fonts/
+cp msyhbd.ttf ~/.wine/drive_c/windows/Fonts/
+regedit zhfont.reg
+rm msyh.ttf
+rm simsum.ttc
+rm zhfont.reg
 clear
-echo "[+] 你好！支持中文的字体看来已经配置好了。我们建议你重启一下电脑，或者是注销一下账户。\n[+] Hello! The Chinese support fonts looks like configured just now. We recommend you to restart your computer or log off your account soon.\n\n[+] 如果有问题，请到https://github.com/sunbossrs/chinese-wine-configure/issue汇报问题。\n[+] If you have any problem on using, you can send reports on https://github.com/sunbossrs/chinese-wine-configure/issue .\n\n[+] 玩的开心！\n[+] Have fun!"
+echo "[+] 你好！支持中文字体的Wine看来已经配置好了。我们建议你重启一下电脑。\n[+] Hello! The Chinese support fonts looks like config successful just now. We recommend you to restart your computer soon.\n\n[+] 如果有问题，请到https://github.com/sunbossrs/chinese-wine-configure/issue汇报。\n[+] If you have any problem on using, you can send reports on https://github.com/sunbossrs/chinese-wine-configure/issue .\n\n[+] 玩的开心！\n[+] Have fun!"
