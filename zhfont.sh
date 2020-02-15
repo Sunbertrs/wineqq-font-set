c520cc5c9e2187d00df681388093d20cc7117d10
SunbossRS <38691765+SunbossRS@users.noreply.github.com>
2020-02-16 00:20:40 +0800

    Update zhfont.sh

--- zhfont.sh
+++ zhfont.sh
@@ -1,14 +1,10 @@
 echo "[+] 现在这个脚本将自动配置Wine的中文字体。请稍候...\n[+] This script is setting up the Chinese fonts of Wine. Please wait..."
 sleep 3
 clear
-wget https://github.com/sunbossrs/chinese-wine-configure/blob/master/fontlib/msyh.ttf?raw=true
-wget https://github.com/sunbossrs/chinese-wine-configure/blob/master/fontlib/simsun.ttc?raw=true
-wget https://github.com/sunbossrs/chinese-wine-configure/blob/master/zhfont.reg?raw=true
-cp msyh.ttf ~/.wine/drive_c/windows/Fonts/
-cp simsun.ttc ~/.wine/drive_c/windows/Fonts/
-regedit zhfont.reg
-rm msyh.ttf
-rm simsun.ttc
-rm zhfont.reg
+cd ~/.wine/drive_c/windows/Fonts
+wget https://github.com/sunbossrs/chinese-wine-configure/blob/master/light-winfonts-lib.tar.xz?raw=true
+tar -xvJf light-winfonts-lib.tar.xz
+rm light-winfonts-lib.tar.xz
+cd ~/
 clear
-echo "[+] 你好！支持中文字体的Wine看来已经配置好了。我们建议你重启一下电脑。\n[+] Hello! The fonts that support Chinese looks like config successful just now. We recommend you to restart your computer soon.\n\n[+] 如果有问题，请到https://github.com/sunbossrs/chinese-wine-configure/issue汇报。\n[+] If you have any problem on using, you can send reports on https://github.com/sunbossrs/chinese-wine-configure/issue .\n\n[+] 玩的开心！\n[+] Have fun!"
+echo "[+] 你好！支持中文字体的Wine看来已经配置好了！\n[+] Hello! The fonts that support Chinese looks like config successful just now!\n\n[+] 如果有问题，请到https://github.com/sunbossrs/chinese-wine-configure/issue汇报。\n[+] If you have any problem on using, you can send reports on https://github.com/sunbossrs/chinese-wine-configure/issue .\n\n[+] 玩的开心！\n[+] Have fun!"
