ba0ef1aa03c983c640abc3fd82d1f7065cd54634
SunbossRS <38691765+SunbossRS@users.noreply.github.com>
2023-03-10 22:52:49 +0800

    Update zhfont.sh

--- zhfont.sh
+++ zhfont.sh
@@ -1,9 +1,6 @@
-echo "[+] 请稍候...现在将自动配置Wine的中文字体。"
+echo "[+] 请稍候..."
 cd ~/.wine/drive_c/windows/Fonts
-wget https://github.com/sunbossrs/cn-wineqq-conf/blob/master/light-winfonts-lib.tar.xz?raw=true
-tar -xvJf light-winfonts-lib.tar.xz
-rm light-winfonts-lib.tar.xz
-cd ~/
+curl -O https://github.com/sunbossrs/cn-wineqq-conf/raw/master/fontlib/msyh.ttf
+curl -O https://github.com/sunbossrs/cn-wineqq-conf/raw/master/fontlib/simsun.ttc
 clear
-echo "[+] 支持中文字体的Wine已经配置好了！\n"
-echo "[+] 如果有问题，请到https://github.com/sunbossrs/cn-wineqq-conf/issue汇报。祝您玩的开心！"
+echo "[+] 配置完成。如果有问题，请到https://github.com/sunbossrs/wineqq-font-set/issue汇报。祝您玩的开心！"
